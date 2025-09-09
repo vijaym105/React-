ca6940a79f14bd8571e2f47546438ee8fa804372
vijaym105 <malusarevijay10@gmail.com>
2025-09-09 17:32:02 +0530

    Card

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,11 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx';
-import Food from './Food.jsx';
+import Card from "./Card";
 
 function App(){
     return(
         <>
-        <Header/>
-        <Food />
-        <Food />
-        <Footer />
-        </>
+     <Card />
+    <Card />
+     </>
     );
 }
 
--- my-react-app/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(
-        <footer>
-            <p>&copy: {new Date().getFullYear()} Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo beatae veritatis hic repellendus! Eum, deserunt.</p>
-        </footer>
-    )
-}
-
-export default Footer
