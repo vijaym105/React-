0409289a4c280e34a8602228dd0c5b2000e1d410
vijaym105 <malusarevijay10@gmail.com>
2025-09-11 18:02:22 +0530

    Conditional Rendering

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,12 @@
+import Login from "./Login";
 
-import Student from "./Student";
 
 function App(){
     return(
         <>
-     <Student name="Vijay" age={20} isstudent={false}/>
-     <Student name="sumit" age={25} isstudent={true}/>
-     <Student name="Vijay" age={20} isstudent={false}/>
-     <Student name="hell"/>
+     <Login isLogin = {true} />
      </>
-    );
-}
+    )
+};
 
 export default App
