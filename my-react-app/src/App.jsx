96f99261b5e3ef4f1ae19f71ab1ce3c032aa822f
vijaym105 <malusarevijay10@gmail.com>
2025-09-15 18:55:17 +0530

    Update obj/array/arr of obj in state

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,25 +1,10 @@
-import Todolist from "./Todolist"
+import Update from "./Update"
+
 
 function App(){
-     const myTodos = [
-    { id: 1, task: "Learn React props", completed: true },
-    { id: 2, task: "Practice conditional rendering", completed: true },
-    { id: 3, task: "Build todo app", completed: false },
-  ];
-return(
-       <>
-  {myTodos.length === 0 ? (
-    <p>No task</p>
-  ) : (
-    <>
-      <h2>This is my todolist</h2>
-      <Todolist todos={myTodos} />
-    </>
-  )}
-</>
-       
-    )
-};
+  return(
+<Update />
+  )
+}
 
 export default App
-
