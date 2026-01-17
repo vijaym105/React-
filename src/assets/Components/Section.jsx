c74807357500de6df4bd6ce3ae7b880c2623ef2c
vijaym105 <malusarevijay10@gmail.com>
2026-01-17 16:27:46 +0530

    Task 1

---
+++ src/assets/Components/Section.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <div className='sec'>
+        <img src="https://images.unsplash.com/photo-1623998021722-b934f73081df?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="not found" />
+        <div className="hero">
+            <h3>Unleash your inner champion today. <br /> all in one place</h3>
+        </div>
+        <div className="para">
+            <p>Join the ultimate tenis experience -- where the passion meets performance, and every swing brings you closer to victory.</p>
+        </div>
+        <div className="btn">
+            <button>Start your journey</button>
+        </div>
+    </div>
+  )
+}
+
+export default Section
